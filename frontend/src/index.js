44ab4c689d3b990735b879612ae4595e508b3a72
AmiyaI <119825168+AmiyaI@users.noreply.github.com>
2024-12-17 20:16:23 -0500

    Frontend changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
-import "./styles.css"; // Make sure this file exists and is correctly referenced
+import App from "./App";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
