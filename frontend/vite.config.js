b722bdcf34e2a2e951994aeb7bf616bee6ab2105
AmiyaI <119825168+AmiyaI@users.noreply.github.com>
2024-12-17 20:56:16 -0500

    Frontend changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Correct output directory
+    outDir: "dist", // Build output folder
   },
   server: {
     port: 3000,
